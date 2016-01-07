4136ca5c443dbbd76c7ad4bd509565d734e7b969
Adam <a.tavvy@gmail.com>
2016-01-07 13:50:54 +0000

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GPM-Playlister
+A command line Node.js tool that generates a Google Music playlist from a BBC Playlister url
---
+++ config/stations.js
@@ -0,0 +1,9 @@
+
+'use strict';
+
+var presetStations = {
+	1: 'http://www.bbc.co.uk/radio1/playlist',
+	2: 'http://www.bbc.co.uk/radio2/playlist'
+};
+
+module.exports = presetStations;
